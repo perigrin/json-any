34965f9f6daecad7b57c4e2898cdc34a436d3674
Chris Prather <chris@prather.org>
2011-09-17 20:17:03 -0400

    rework to use a similar process as Any::Moose so that the toolchain bits work properly

---
+++ inc/MakeMaker.pm
@@ -0,0 +1,41 @@
+package inc::MakeMaker;
+use Moose;
+extends 'Dist::Zilla::Plugin::MakeMaker::Awesome';
+
+use namespace::autoclean;
+
+override _build_WriteMakefile_dump => sub {
+    my ($self) = @_;
+
+    my $str = super;
+
+    $str .= ";\n\n";
+
+    $str .= <<'END_NONSENSE';
+  
+    sub has_json () {
+        our @order = qw(XS JSON DWIW);
+        foreach my $testmod (@order) {
+            $testmod = "JSON::$testmod" unless $testmod eq "JSON";
+            eval "require $testmod";
+            return 1 unless $@;
+        }
+        return 0;
+    }
+
+    if (has_json) {
+        # we have some kind of supported JSON module, we're good
+    }
+    else {
+
+        # we need to have a version of JSON, go with JSON.pm as a sane default
+        $WriteMakefileArgs{PREREQ_PM}{JSON} = '2.02';
+    }
+    
+  
+END_NONSENSE
+
+    return $str;
+};
+
+1;
